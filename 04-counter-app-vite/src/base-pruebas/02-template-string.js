53148239ecee6b3ee99db8fada78190e2cc2b410
Contreras <Sebastian.Contreras@btgpactual.com>
2023-07-21 17:02:35 -0500

    Inicio seccion 9 profundizando hooks

--- 04-counter-app-vite/src/base-pruebas/02-template-string.js
+++
@@ -1,6 +0,0 @@
-export function getSaludo(nombre) {
-    return 'Hola ' + nombre + '!!!';
-}
-
-const nombre='Sebas'
-console.log(`Este es un texto: ${getSaludo(nombre)}`)
--- 04-counter-app-vite/src/base-pruebas/08-import-export.js
+++
@@ -1,9 +0,0 @@
-import heroes from "../data/heroes";
-
-export const getHeroById = (id) => {
-    return heroes.find(heroe => heroe.id===id);
-}
-
-export const getHeroByOwner = (owner) => {
-    return heroes.filter(heroe => heroe.owner===owner);
-}
